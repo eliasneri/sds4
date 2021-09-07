7db68e61dd1c3fef1747c42589906d6fc22f3539
elias neri <eliasneri@hotmail.com>
2021-09-07 16:33:36 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
 
 function App() {
   return (
-  <div>
-    <h1>Olá Mundo!</h1>
-  </div>  );
+    <>
+       <Navbar />
+       <div className="container">
+         <h2 className="text-primary">Olá Mundo</h2>
+         <DataTable />
+         
+       </div>
+        <Footer />
+   </>
+  );
 }
 
 export default App;
