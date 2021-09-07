3b9deb8d9250fdf4b9d05d3afc030a65236fa63b
elias neri <eliasneri@hotmail.com>
2021-09-07 17:34:21 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
 import DataTable from 'components/DataTable';
+import Barchart from 'components/BarChart';
+import Donutchart from 'components/DonutChart';
 
 function App() {
   return (
     <>
        <Navbar />
        <div className="container">
-         <h2 className="text-primary">Olá Mundo</h2>
+         <h2 className="text-primary py-3">DashBoard de Vendas</h2>
+            <div className="row px-3">
+                <div className="col-sm-6">
+                    <h5>Porcentagem de Sucesso</h5>
+                    <Barchart />
+                </div>
+
+                <div className="col-sm-6">
+                    <h5>Porcentagem da Rosca</h5>
+                    <Donutchart />
+                </div>
+
+
+            </div>
+         
          <DataTable />
          
        </div>
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <strong> <a href="https://github.com/eliasneri" target="_blank" rel="noreferrer" />Elias Néri - 2021 </strong></p>
+                <p className="text-light">App desenvolvido por <strong> <a href="https://github.com/eliasneri" target="_blank" rel="noreferrer">Elias Néri - 2021</a> </strong></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
