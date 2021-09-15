078d5fa52448921b06392ec37780950edf74b80f
eliasneri <eliasneri@hotmail.com>
2021-09-15 15:51:23 -0300

    DataTable Integration

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+
 export const round = (value: number, precision: number) => {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
