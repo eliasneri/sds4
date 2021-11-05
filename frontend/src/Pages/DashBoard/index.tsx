301ac3dc237125328336ac0143e22b9c0a55828d
eliasneri <eliasneri@hotmail.com>
2021-11-05 15:00:10 -0300

    Pagination

--- frontend/src/Pages/DashBoard/index.tsx
+++ frontend/src/Pages/DashBoard/index.tsx
@@ -5,31 +5,30 @@ import DataTable from '../../components/DataTable';
 import Barchart from '../../components/BarChart';
 import Donutchart from '../../components/DonutChart'
 
-const DashBoard =() =>{
-    return(
+const DashBoard = () => {
+    return (
         <>
-       <Navbar />
-       <div className="container">
-         <h2 className="text-primary py-3">DashBoard de Vendas</h2>
-            <div className="row px-3">
-                <div className="col-sm-6">
-                    <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
-                    <Barchart />
+            <Navbar />
+            <div className="container">
+                <h2 className="text-primary py-3">DashBoard de Vendas</h2>
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+                        <Barchart />
+                    </div>
+
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Todas as Vendas</h5>
+                        <Donutchart />
+                    </div>
                 </div>
-
-                <div className="col-sm-6">
-                    <h5 className="text-center text-secondary">Todas as Vendas</h5>
-                    <Donutchart />
-                </div>
-
-
+            
+                <h2 className="text-primary py-3">Todas as Vendas</h2>
+                <DataTable />
+            
             </div>
-            <h2 className="text-primary py-3">Todas as Vendas</h2>
-         <DataTable />
-         
-       </div>
-        <Footer />
-   </>
+            <Footer />
+        </>
     );
 }
 
